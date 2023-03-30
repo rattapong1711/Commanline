c68970fc869780eef562ad26855ea0556076d9ae
rattapong panjaroen <rattapong.p@kkumail.com>
2023-03-31 01:38:30 +0700

    Commanline

---
+++ Lab-OOP-00A-main/Lab-OOP-00A/Lab-OOP-00A/Program.cs
@@ -0,0 +1,11 @@
+﻿//Fname&Lname
+Console.Write("My name is Rattapong Panjaroen\n");
+
+//St_Id
+Console.Write("My ID is 643450650-3\n");
+
+//kkumail
+Console.Write("My kkumail is rattapong.p@kkumail.com\n");
+
+//quote
+Console.Write("To day is not Yesterday");
